4fb14677b7d8833deb3c65f74b85e43c5bed50eb
Daniel Talhaug <daniel.talhaug@outlook.com>
2024-03-04 00:15:28 +0100

    Install & basic setup for Tresjs

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-import Button from 'primevue/button';
+import CardEditor from '@/components/CardEditor.vue';
 
 </script>
 
 <template>
     <main>
-        <div class="card flex justify-content-center flex-wrap gap-3">
-            <Button label="Primary" />
-            <Button label="Secondary" severity="secondary" />
-            <Button label="Success" severity="success" />
-            <Button label="Info" severity="info" />
-            <Button label="Warning" severity="warning" />
-            <Button label="Help" severity="help" />
-            <Button label="Danger" severity="danger" />
-            <Button label="Contrast" severity="contrast" />
-        </div>
+        <CardEditor />
     </main>
 </template>
 
