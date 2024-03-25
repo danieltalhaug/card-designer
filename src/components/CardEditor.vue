da74bddf9869661be446ddf126d2e22f32cfd18a
Daniel Talhaug <daniel.talhaug@outlook.com>
2024-03-25 17:10:45 +0100

    Allow model to cast shadow

--- src/components/CardEditor.vue
+++ src/components/CardEditor.vue
@@ -11,6 +11,11 @@ const { gridHelperPosition, gridHelperArgs } = useSettingsGridHelper();
 </script>
 
 <template>
+    <TresCanvas clear-color="#82DBC5" shadows window-size>
+        <TresMesh cast-shadow>
+            <TresTorusGeometry :args="[0.5, 0.25, 16, 64]" />
+            <TresMeshToonMaterial color="orange" />
+        </TresMesh>
         <TresMesh
             :position="[0, -1, 0]"
             :rotation="[-1.571, 0, 0]"
@@ -28,6 +33,12 @@ const { gridHelperPosition, gridHelperArgs } = useSettingsGridHelper();
             :auto-rotate-speed
         />
         <TresAmbientLight :intensity="1" />
+        <TresDirectionalLight
+            color="white"
+            :position="[0, 50, 0]"
+            :intensity="1"
+            cast-shadow
+        />
         <TresGridHelper
             :position="[...gridHelperPosition]"
             :args="gridHelperArgs"
