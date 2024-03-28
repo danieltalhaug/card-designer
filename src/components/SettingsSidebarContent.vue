77bd416e72953644698a9089475aa7f20b5d2702
Daniel Talhaug <daniel.talhaug@outlook.com>
2024-03-28 20:48:14 +0100

    Store last used setting accordion in localstorage

--- src/components/SettingsSidebarContent.vue
+++ src/components/SettingsSidebarContent.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import Accordion from 'primevue/accordion';
 import AccordionTab from 'primevue/accordiontab';
 import SettingsFormScene from './SettingsFormScene.vue';
 import SettingsFormGeometry from './SettingsFormGeometry.vue';
+import { useLocalStorage } from '@vueuse/core';
+
+const lastUsedAccordion = ref(useLocalStorage('lastUsedAccordionIndex', 0));
 
 const settings = [
-	{header: 'Scene', component: SettingsFormScene},
-	{header: 'Geometry', component: SettingsFormGeometry},
+	{ header: 'Scene', component: SettingsFormScene },
+	{ header: 'Geometry', component: SettingsFormGeometry },
 ];
-
 </script>
 
 <template>
-	<Accordion :active-index="0">
+	<Accordion
+		:active-index="lastUsedAccordion"
+		@tab-open="lastUsedAccordion = $event.index"
+	>
 		<AccordionTab
 			v-for="setting in settings"
 			:key="setting.header"
